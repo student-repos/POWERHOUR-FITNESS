dfd931b6aac3fec6b24f726ab6f434a914346a22
John Doe <johndoe@exam>
2024-06-17 11:35:00 +0200

    first commit for the Testimonials

--- frontend/src/components/Testimonials/Testimonials.jsx
+++ frontend/src/components/Testimonials/Testimonials.jsx
@@ -1,17 +1,96 @@
-import React from 'react';
-import './Testimonials.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faHandPointRight } from "@fortawesome/free-solid-svg-icons";
+import "./Testimonials.css";
+import PrevNextButtons from "./Button.jsx";
 
 function Testimonials() {
+  const [reviews, setReviews] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      const response = await axios.get("http://localhost:7000/review");
+      setReviews(response.data);
+    };
+
+    fetchReviews();
+  }, []);
+
+  const handleReviewClick = () => {
+    navigate("/login");
+  };
+
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : reviews.length - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex < reviews.length - 1 ? prevIndex + 1 : 0
+    );
+  };
+
   return (
-    <section className="testimonials" id="testimonials">
-      <h2>What Our Personal Service Clients Have to Say</h2>
-      <div className="testimonial-list">
-        <div className="testimonial-card">
-          <p>"FitTrack Pro is a user-friendly gym app with an extensive workout library, personalized plans, and a helpful nutrition tracker."</p>
-          <p><strong>Stefanie Gonderman</strong><br />Happy Customer</p>
+    <div className="review-page" id="testimonials">
+      <div className="t-header">
+        <span className="text-1">CUSTOMER</span>
+        <span className="text-2">SATISFACTION</span>
+        <span className="text-3">IS</span>
+        <span className="text-4">OUR GOAL</span>
+        <span className="text-5">
+          To provide you allways better services than before, write us your
+          feedbacks please and rate the app too.
+        </span>
+        <span className="text-6">Tell others what you think</span>
+        <div className="stars" onClick={handleReviewClick}>
+          {[...Array(5)].map((_, index) => (
+            // <span className="static-star" key={index}>⭐</span>
+            <FontAwesomeIcon
+              key={index}
+              icon={faStar}
+              className="static-star"
+            />
+          ))}
         </div>
+        <p className="write-review" onClick={handleReviewClick}>
+          <FontAwesomeIcon
+            icon={faHandPointRight}
+            style={{ color: "#FFD43B", fontSize: "40px", marginRight: "40px" }}
+          />
+          Write your review
+        </p>
       </div>
-    </section>
+      {Array.isArray(reviews) && reviews.length > 0 && (
+        <div className="review-display">
+          <div className="img-container">
+            <img src="path/to/image.jpg" />
+          </div>
+          <div className="review-details">
+            <p className="review-name">{reviews[currentIndex].fullName}</p>
+            <div className="stars">
+              {[...Array(reviews[currentIndex].rating)].map((_, index) => (
+                <span className="dynamic-star" key={index}>
+                  ⭐
+                </span>
+              ))}
+            </div>
+            <p className="review-message">{reviews[currentIndex].message}</p>
+          </div>
+          <div className="slider-container">
+            <PrevNextButtons
+              onPrevClick={handlePrevClick}
+              onNextClick={handleNextClick}
+            />
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
