c94becb2064950b14dc5c1ba56d47e70b23e40be
Dany <danialikhani@gmail.com>
2024-06-17 13:57:27 +0200

    add program page without structring

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,26 +4,28 @@ const userSchema = new Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    age: { type: Number},
+    age: { type: Number },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     telephone: { type: String },
     verified: { type: Boolean, default: false },
-    role: { type: String, enum: ["member", "trainer", "admin"] },
+    role: {
+      type: String,
+      enum: ["member", "trainer", "admin"],
+      default: "member",
+    },
     picture: { type: String },
     address: { type: String },
     trainerType: { type: String, enum: ["yoga", "pilates", "Cardio"] },
-    trainerDescription: { type: String }
+    trainerDescription: { type: String },
   },
-  { timestamps: true },
-  
+  { timestamps: true }
 );
 
 const User = model("User", userSchema);
 
 export default User;
 
-
 userSchema.add({
-  picture: { type: String }
+  picture: { type: String },
 });
