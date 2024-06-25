c18aa748339a1afb638296c716057db8b1788ed9
Dany <danialikhani@gmail.com>
2024-06-25 13:50:06 +0200

    repair the course endpoint and conected to the trainer

--- backend/routes/courseRouter.js
+++ backend/routes/courseRouter.js
@@ -7,9 +7,13 @@ import {
     getCourseById
 } from "../controllers/courseController.js";
 
+import {isAuth} from "../middlewares/isAuth.js";
+import {isTrainer} from "../middlewares/isTrainer.js";
+
+
 const router = Router();
 
-router.post("/", postNewCourse);
+router.post("/",isAuth, isTrainer, postNewCourse);
 router.get("/", getAllCourses);
 router.get("/:id", getCourseById);
 router.put("/:id", updateCourseById);
